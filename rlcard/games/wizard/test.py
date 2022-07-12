d3adda8a9cf7482a35ccc0a482cbbe80064bd9b1
Benedikt Neubauer <benni.neubauer@gmx.net>
2022-07-12 16:56:00 +0200

    updated test logic

--- rlcard/games/wizard/test.py
+++ rlcard/games/wizard/test.py
@@ -1,8 +1,30 @@
 from game import WizardGame
+from rlcard import agents
 from rlcard.agents import RandomAgent
 import rlcard
 import card
 import utils
+import os
+
+from rlcard.utils import (
+    get_device,
+    tournament
+)
+
+from rlcard.agents.human_agents.wizard_human_agent import HumanAgent
+
+
+def load_model(model_path, env=None, position=None, device=None):
+    if os.path.isfile(model_path):  # Torch model
+        import torch
+        agent = torch.load(model_path, map_location=device)
+        agent.set_device(device)
+    elif model_path == 'random':  # Random model
+        from rlcard.agents import RandomAgent
+        agent = RandomAgent(num_actions=env.num_actions)
+
+    return agent
+
 
 if __name__ == '__main__':
     # env = WizardGame()
@@ -16,12 +38,24 @@ if __name__ == '__main__':
         'wizard',
         config={
             'seed': 42,
-            'game_num_players': 4
+            'game_num_players': 3,
+            'game_num_rounds': 20
         }
     )
 
-    agents = [RandomAgent(num_actions=wizardEnv.num_actions) for _ in range(wizardEnv.num_players)]
+    device = get_device()
 
-    wizardEnv.set_agents(agents)
+    # human_agent = HumanAgent(num_actions=wizardEnv.num_actions)
+    dqn_agent = load_model(
+        "final_models/dqn_wizard_player_0/model_round20.pth", wizardEnv, 0, device)
+    random1 = RandomAgent(num_actions=wizardEnv.num_actions)
+    random2 = RandomAgent(num_actions=wizardEnv.num_actions)
 
-    wizardEnv.run(is_training=False)
+    wizardEnv.set_agents([dqn_agent, random1, random2])
+
+    # trajectories, payoffs = wizardEnv.run(is_training=False)
+    # print("Trajectory", trajectories[-1][-1])
+    # print("Payoffs:", payoffs)
+
+    rewards = tournament(wizardEnv, 100)
+    print("Rewards:", rewards)
